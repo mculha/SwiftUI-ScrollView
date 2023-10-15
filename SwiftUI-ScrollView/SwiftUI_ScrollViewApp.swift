1f6988f1454ca03e872ca4bb74e999c8f9ebb984
mculha <melih.culha.09@gmail.com>
2023-10-15 21:47:30 +0300

    Initial Commit

---
+++ SwiftUI-ScrollView/SwiftUI_ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ScrollViewApp.swift
+//  SwiftUI-ScrollView
+//
+//  Created by Melih Çulha on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
